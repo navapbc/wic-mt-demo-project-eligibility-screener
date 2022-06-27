ef8a832d92bf30206da6a434d6838520b97f8006
Alsia Plybeah <alsia@navapbc.com>
2022-06-27 14:39:29 -0400

    remove bad bucket policy

--- infra/template/iam.tf
+++ infra/template/iam.tf
@@ -6,12 +6,15 @@
 #
 # ----------------------------------------------------------
 
-resource "aws_iam_role" "wic_mt_dev" {
+# resource "aws_iam_role" "wic_mt_dev" {
   
-}
+# }
+
 
 # ----------------------------------------------------------
 # 
 # IAM Roles for FARGATE
 #
 # ----------------------------------------------------------
+# resource "aws_iam_role" "wic_mt_bucket_management" {
+# }
--- infra/template/s3.tf
+++ infra/template/s3.tf
@@ -3,6 +3,7 @@
 # bucket for hosting
 resource "aws_s3_bucket" "wic_mt_tf_state" {
   bucket = "wic-mt-tf-state"
+  force_destroy = true
 }
 
 # encrypt data
@@ -25,9 +26,6 @@ resource "aws_s3_bucket_public_access_block" "wic_mt_tf_state" {
   restrict_public_buckets = true
 }
 
-resource "aws_iam_role" "wic_mt_bucket_management" {
-}
-
 
 # create iam policy for bucket
 data "aws_iam_policy_document" "wic_mt_tf_state_policy" {
@@ -46,13 +44,9 @@ data "aws_iam_policy_document" "wic_mt_tf_state_policy" {
     resources = [ aws_s3_bucket.wic_mt_tf_state.arn, "${aws_s3_bucket.wic_mt_tf_state.arn}/*" ]
   }
 }
-resource "aws_iam_policy" "wic_mt_tf_state_policy" {
-  name = "tf_state_policy"
-  path = "/"
-  policy = data.aws_iam_policy_document.wic_mt_tf_state_policy.json
-}
+
 # add policy to bucket
 resource "aws_s3_bucket_policy" "wic_mt_tf_state_bucket"{
-  bucket = aws_s3_bucket.wic_mt_tf_state_bucket.id
-  policy = aws_iam_policy.wic_mt_tf_state_policy.json
+  bucket = aws_s3_bucket.wic_mt_tf_state.id
+  policy = data.aws_iam_policy_document.wic_mt_tf_state_policy.json
 }
