b93ec6e2aae747f078cfdc930ac403348985d8c8
Rocket ! <rocketnova@users.noreply.github.com>
2022-07-14 18:46:31 -0700

    Copy over from Application Bootstrap template repo.
    - USWDS + Storybook + Sass: https://github.com/navapbc/template-application-nextjs/pull/15
    - i18n for Next.js, Storybook, Jest: https://github.com/navapbc/template-application-nextjs/pull/24

---
+++ app/tests/pages/index.test.tsx
@@ -0,0 +1,23 @@
+// test/pages/index.test.js
+
+import { render, screen } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import Home from '@pages/index'
+
+describe('Home', () => {
+  it('should render the heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByText(/Welcome!/i)
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toMatchSnapshot()
+  })
+
+  it('should pass accessibility scan', async () => {
+    const { container } = render(<Home />)
+    const results = await axe(container)
+
+    expect(results).toHaveNoViolations()
+  })
+})
