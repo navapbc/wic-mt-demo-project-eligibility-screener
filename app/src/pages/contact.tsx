265db095a8279f092990d6ac5104ededb5d4c452
karinamzalez <karinamzalez@gmail.com>
2022-07-18 00:49:25 -0500

    contact: add contact page basic styling and copy

--- app/src/pages/contact.tsx
+++ app/src/pages/contact.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import type { GetServerSideProps, NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
@@ -6,14 +7,23 @@ import ButtonLink from '../components/ButtonLink'
 
 const Contact: NextPage = () => {
   const { t } = useTranslation('common')
-  const listCopyKeys: string[] = ['apply', 'eligible', 'appointment']
 
   return (
     <>
-      <h2>{t('Contact.firstName')}</h2>
-      <h2>{t('Contact.lastName')}</h2>
-      <h2>{t('Contact.phone')}</h2>
-      <h2>{t('Contact.other')}</h2>
+      <Label className="usa-label" htmlFor="input-type-text">{t('Contact.firstName')}</Label>
+      <Input className="usa-input" />
+      <br />
+      <Label className="usa-label" htmlFor="input-type-text">{t('Contact.lastName')}</Label>
+      <Input className="usa-input" />
+      <br />
+      <Label className="usa-label" htmlFor="input-type-text">{t('Contact.phone')}</Label>
+      <Input className="usa-input" />
+      <Helper>{t('Contact.phoneHelper')}</Helper>
+      <br />
+      <Label className="usa-label" htmlFor="input-type-textarea">{t('Contact.other')}</Label>
+      <Input className="usa-input" />
+      <br />
+      <br />
       <ButtonLink
         href="/"
         label={t('Contact.button')}
@@ -22,6 +32,25 @@ const Contact: NextPage = () => {
     </>
   )
 }
+
+const Helper = styled.div`
+  color: #666666;
+  font-size: 12px;
+  max-width: 30rem;
+  padding: 9px;
+}
+`
+
+const Input = styled.input`
+  border: 1px solid #B3B3B3;
+  border-radius: 4px;
+  height: 48px;
+`
+
+const Label = styled.label`
+  font-family: 'Balsamiq Sans',cursive;
+  font-size: 20px;
+`
 
 export const getServerSideProps: GetServerSideProps = async({ locale }) => {
   return {
