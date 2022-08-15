98151f607e4a478c3671115fea98e199a737bb7a
Alsia Plybeah <alsia@navapbc.com>
2022-08-15 11:44:59 -0400

    update docker compose and create workflow

--- infra/template/iam.tf
+++ infra/template/iam.tf
@@ -9,7 +9,7 @@ resource "aws_iam_user" "github_actions" {
 }
 resource "aws_iam_role" "github_actions" {
   name = "deployment-action"
-  assume_role_policy = ""
+  assume_role_policy = aws_iam_user.github_actions.arn
 }
 
 data "aws_iam_policy_document" "github_actions" {
@@ -25,3 +25,26 @@ data "aws_iam_policy_document" "github_actions" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role = aws_iam_role.github_actions.name
+  arn  = aws_iam_policy.github_actions.arn
+}
+
+# add ecr perms (push)
+data "aws_iam_role_policy_document" "github_actions" {
+ statement {
+    sid   = "WICUpdateECR"
+    actions = ["ecs:UpdateCluster", "ecs:UpdateService", "ecr:*"]
+    principals {
+      type  = "AWS"
+      identifiers = [
+        aws_iam_user.github_actions.arn
+      ]
+    }
+  } 
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name = "wic-mt-deploy"
+  policy = data.aws_iam_role_policy_document.github_actions
+}
