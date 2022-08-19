a91a7d0cb026fe594a28ee33fa728dd61b5ecfa0
Alsia Plybeah <alsia@navapbc.com>
2022-08-19 16:22:46 -0400

    Alsia/WMDP-163 Add CD to the screener (#67)
    Ticket
    https://wicmtdp.atlassian.net/browse/WMDP-164
    https://wicmtdp.atlassian.net/browse/WMDP-163
    
    Changes
    What was added, updated, or removed in this PR.
    
    Context for reviewers
    CD should:
    Build the image
    Store the image in ECR
    Run aws ecs update-service --cluster --service --force-new-deployment after we confirm the image is in ECR in order to force the update
    
    There is a commented-out iam.tf which shows the roles and permissions created as a part of getting this action running. These will live in the wic-mt-demo repo.

--- infra/template/iam.tf
+++ infra/template/iam.tf
@@ -1,67 +1,53 @@
-# configurations for iam roles go here
-
 # ----------------------------------------------------------
 # 
 # IAM Roles for Users
 #
 # ----------------------------------------------------------
+# IAM roles/users created here:
+# wic-mt-github-actions: this is the user that will hold temporary credentials to AWS when the deploy action is running
+# deployment-action: gives the user permission to perform all ECR services (will be narrowed down later), and Update actions for ECS
 
-# resource "aws_iam_role" "wic_mt_dev" {
-  
+# resource "aws_iam_user" "github_actions" {
+#   name = "wic-mt-github-actions"
+# }
+# resource "aws_iam_role" "github_actions" {
+#   name = "deployment-action"
+#   assume_role_policy = aws_iam_user.github_actions.arn
 # }
 
-
-# ----------------------------------------------------------
-# 
-# IAM Roles for FARGATE
-#
-# ----------------------------------------------------------
-
-# Allows an IAM role to perform ECS tasks
-data "aws_iam_policy_document" "ecs_assume_role_policy" {
-  statement {
-    sid = "ECSTaskExecution"
-    actions = [
-      "sts:AssumeRole"
-    ]
-    effect = "Allow"
-
-    principals {
-      type = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecs_executor" {
-  name = "wic-mt-task-executor"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
-}
-
-# For a later ticket: This policy allows access to parameter store secrets. Uncomment and add "resources" field
-# data "aws_iam_policy_document" "ecs_access_policy" {
+# data "aws_iam_policy_document" "github_actions" {
 #   statement {
-#     sid = "LogToCloudwatch"
-#     actions = [
-#       "logs:CreateLogStream",
-#       "logs:PutLogEvents",
-#       "logs:DescribeLogStreams"
-#     ]
+#     sid   = "WICDeploymentAssumeRole"
+#     actions = ["sts:AssumeRole", "sts:TagSession"]
+#     principals {
+#       type  = "AWS"
+#       identifiers = [
+#         aws_iam_user.github_actions.arn
+#       ]
+#     }
 #   }
-
-#   statement {
-#     sid = "AccessSecrets"
-#     actions = [
-#       "ssm:GetParameter",
-#       "ssm:GetParameters",
-#     ]
-#   }
-  
 # }
 
-# resource "aws_iam_role_policy" "task_access_policy" {
-#   name = "wic-mt-task-access"
-#   role = aws_iam_role.ecs_executor.id
-#   policy = data.aws_iam_policy_document.ecs_access_policy.json
-  
+# resource "aws_iam_role_policy_attachment" "github_actions" {
+#   role = aws_iam_role.github_actions.name
+#   policy_arn  = aws_iam_policy.deploy_action.arn
+# }
+
+# # add ecr perms (push)
+# data "aws_iam_policy_document" "deploy_action" {
+#  statement {
+#     sid   = "WICUpdateECR"
+#     actions = ["ecs:UpdateCluster", "ecs:UpdateService", "ecr:*"]
+#     principals {
+#       type  = "AWS"
+#       identifiers = [
+#         aws_iam_user.github_actions.arn
+#       ]
+#     }
+#   } 
+# }
+
+# resource "aws_iam_policy" "deploy_action" {
+#   name = "wic-mt-deploy"
+#   policy = data.aws_iam_policy_document.deploy_action.json
 # }
--- infra/template/main.tf
+++ infra/template/main.tf
@@ -19,18 +19,6 @@ terraform {
   }
 }
 
-# dynamodb table to support state locking
-resource "aws_dynamodb_table" "tf_state_table" {
-  name = "wic_terraform_locks"
-  hash_key = "LockID"
-  read_capacity = 1
-  write_capacity = 1
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
   profile = "wic-mt"
