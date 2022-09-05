b5dbb2ac03b0ccb48ffde3fb489024b733897b8f
karinamzalez <karinamzalez@gmail.com>
2022-09-05 18:43:08 -0500

    run pretiter

--- app/src/components/Alert.tsx
+++ app/src/components/Alert.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 
 type Props = {
-  text: string,
+  text: string
   type: 'error' | 'info' | 'success' | 'warning'
 }
 
@@ -11,9 +11,7 @@ const Alert = (props: Props): ReactElement => {
   return (
     <div className={`usa-alert usa-alert--${type}`} role="alert">
       <div className="usa-alert__body">
-        <p className="usa-alert__text">
-          {text}
-        </p>
+        <p className="usa-alert__text">{text}</p>
       </div>
     </div>
   )
