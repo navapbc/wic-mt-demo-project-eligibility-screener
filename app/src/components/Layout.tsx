476d1b0caa7a793960d31f613047c88323b3c95e
Rocket ! <rocketnova@users.noreply.github.com>
2022-09-15 19:03:42 -0700

    Theme page layout using USWDS grid and utilities. Set header nav to be full width.

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Trans, useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import { ReactElement } from 'react'
-import styled from 'styled-components'
 
 type Props = {
   children: ReactElement
@@ -12,61 +11,72 @@ const Layout = ({ children }: Props): ReactElement => {
 
   return (
     <div className="container">
-      <header className="usa-header usa-header--basic">
-        <div className="usa-nav-container">
-          <div className="usa-navbar">
-            <div className="usa-logo">
-              <em className="usa-logo__text">{t('Layout.header')}</em>
+      <header className="header usa-header usa-header--basic">
+        <div className="usa-navbar">
+          <div className="grid-row">
+            <div className="desktop:grid-col-8 desktop:grid-offset-2">
+              <div className="usa-logo margin-left-2">
+                <em className="usa-logo__text">{t('Layout.header')}</em>
+              </div>
             </div>
           </div>
         </div>
       </header>
-      <main className="main">{children}</main>
-      <footer className="footer">
-        <Logos>
-          <Image
-            src="/img/wic-logo.svg"
-            alt="WIC logo"
-            width={64.52}
-            height={32}
-          />
-          <Image
-            src="/img/montana-logo.svg"
-            alt="Monthana DPHHS logo"
-            width={46.22}
-            height={32}
-          />
-        </Logos>
-        <br />
-        <div>
-          <Trans
-            components={[
-              <a key="0" href="https://dphhs.mt.gov/ecfsd/wic/index" />,
-              <a key="1" href="https://www.signupwic.com/" />,
-            ]}
-            i18nKey={'Layout.footer1'}
-            t={t}
-          />
-          <br />
-          <br />
-          <Trans
-            components={[
-              <a
-                key="0"
-                href="https://www.fns.usda.gov/civil-rights/usda-nondiscrimination-statement-other-fns-programs"
-              />,
-            ]}
-            i18nKey={'Layout.footer2'}
-            t={t}
-          />
+      <main className="main">
+        <div className="grid-row">
+          <div className="desktop:grid-col-8 desktop:grid-offset-2  padding-2">
+            {children}
+          </div>
+        </div>
+      </main>
+      <footer className="footer usa-footer usa-footer--slim">
+        <div className="usa-footer__primary-section">
+          <div className="grid-row">
+            <div className="desktop:grid-col-8 desktop:grid-offset-2 padding-2">
+              <div className="logos">
+                <Image
+                  src="/img/wic-logo.svg"
+                  alt="WIC logo"
+                  width={64.52}
+                  height={32}
+                />
+                <Image
+                  src="/img/montana-logo.svg"
+                  alt="Monthana DPHHS logo"
+                  width={46.22}
+                  height={32}
+                />
+              </div>
+              <div className="font-body-3xs">
+                <p>
+                  <Trans
+                    components={[
+                      <a key="0" href="https://dphhs.mt.gov/ecfsd/wic/index" />,
+                      <a key="1" href="https://www.signupwic.com/" />,
+                    ]}
+                    i18nKey={'Layout.footer1'}
+                    t={t}
+                  />
+                </p>
+                <p>
+                  <Trans
+                    components={[
+                      <a
+                        key="0"
+                        href="https://www.fns.usda.gov/civil-rights/usda-nondiscrimination-statement-other-fns-programs"
+                      />,
+                    ]}
+                    i18nKey={'Layout.footer2'}
+                    t={t}
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
   )
 }
 
-const Logos = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 80px);
-`
 export default Layout
