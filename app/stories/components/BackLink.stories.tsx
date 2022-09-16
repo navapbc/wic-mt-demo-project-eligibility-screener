3cd2c6bad304e9a3b02ceb38466bb78e47d4f656
Rocket ! <rocketnova@users.noreply.github.com>
2022-09-16 12:27:50 -0700

    Create a BackLink component.

---
+++ app/stories/components/BackLink.stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react'
+
+import { BackLink as BackLinkComponent, BackLinkProps } from '@components/BackLink'
+
+export default {
+  title: 'Components',
+  component: BackLinkComponent,
+} as Meta
+
+const Template: Story<BackLinkProps> = (args) => <BackLinkComponent {...args} />
+
+export const BackLink = Template.bind({})
+
+BackLink.args = {
+  href: 'test.com',
+}
