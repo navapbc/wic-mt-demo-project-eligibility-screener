7ec208278627ebb00bd10281e351e8a48a0ffc9c
karinamzalez <karinamzalez@gmail.com>
2022-09-20 13:01:29 -0500

    remove dead code

--- app/src/pages/eligibility.tsx
+++ app/src/pages/eligibility.tsx
@@ -83,7 +83,7 @@ const Eligibility: NextPage<Props> = (props: Props) => {
       </p>
       <InputChoiceGroup
         required
-        title={`1. ${t('Eligibility.residential')}`}
+        title={t('Eligibility.residential')}
         type="radio"
         choices={[
           {
@@ -109,7 +109,7 @@ const Eligibility: NextPage<Props> = (props: Props) => {
           header: t('Eligibility.accordionHeader'),
         }}
         required
-        title={`2. ${t('Eligibility.categorical')}`}
+        title={t('Eligibility.categorical')}
         type="checkbox"
         choices={[
           {
