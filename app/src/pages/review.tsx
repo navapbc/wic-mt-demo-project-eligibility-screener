b2351f0ec821197a9a05429557a5e80cfd457e09
karinamzalez <karinamzalez@gmail.com>
2022-09-23 13:20:02 -0500

    write test api POST call

--- app/src/pages/review.tsx
+++ app/src/pages/review.tsx
@@ -20,6 +20,29 @@ const Review: NextPage = () => {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const res = await fetch(`${process.env.HOST}/v1/eligibility-screener`, {
+    method: 'POST',
+    body: JSON.stringify({
+      first_name: "Jane",
+      last_name: "Doe",
+      phone_number: "111-111-1111",
+      eligibility_categoies: ["pregnant"],
+      has_prior_wic_enrollment: false,
+      eligibility_programs: ["snap"],
+      household_size: 2,
+      zip_code: "11111",
+      wic_clinic: "Example Clinic",
+      applicant_notes: ""
+    }),
+    headers: {
+      'Content-type': 'application/json',
+      'X-Auth': 'LOCAL_AUTH_12345678'
+    }
+  })
+  const response = await res.json()
+  console.log("TESTINGG")
+  console.log(response)
+  debugger
   return {
     props: {
       ...(await serverSideTranslations(locale || 'en', ['common'])),
