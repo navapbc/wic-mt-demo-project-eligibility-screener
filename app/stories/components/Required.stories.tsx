43ca1d011d00241b053cb0b3711452e8febfc717
karinamzalez <karinamzalez@gmail.com>
2022-10-09 13:32:45 -0500

    add Required story

---
+++ app/stories/components/Required.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react'
+
+import { Required as RequiredComponent } from '@components/Required'
+
+export default {
+  title: 'Components',
+  component: RequiredComponent,
+} as Meta
+
+const Template: Story = (args) => (
+  <RequiredComponent {...args} />
+)
+
+export const Required = Template.bind({})
