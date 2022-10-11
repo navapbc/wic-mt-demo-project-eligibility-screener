688b188fc0327206e14bd4d48ba7ada85db2eb17
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-11 15:59:02 -0700

    Button: next/link reires using React.forwardRef for child functional components.

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -1,5 +1,5 @@
 import { Trans } from 'next-i18next'
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 
 type Props = {
   disabled?: boolean
@@ -8,25 +8,31 @@ type Props = {
   onClick?: () => void
 }
 
-const Button = (props: Props): ReactElement => {
-  const { disabled, labelKey, style, onClick } = props
+// Next.js requires forwarding refs if this functional component might
+// be a child component of next/link. For more info, see
+// https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-function-component
+const Button = React.forwardRef(
+  (props: Props, ref: React.Ref<HTMLDivElement>): ReactElement => {
+    const { disabled, labelKey, style, onClick } = props
 
-  let buttonStyle = ''
-  if (style) {
-    buttonStyle = `usa-button--${style} margin-top-1`
-  } else {
-    buttonStyle = 'margin-top-6'
+    let buttonStyle = ''
+    if (style) {
+      buttonStyle = `usa-button--${style} margin-top-1`
+    } else {
+      buttonStyle = 'margin-top-6'
+    }
+
+    return (
+      <button
+        className={`usa-button usa-button--small display-block ${buttonStyle}`}
+        disabled={disabled}
+        onClick={onClick}
+        ref={ref}
+      >
+        <Trans i18nKey={labelKey} />
+      </button>
+    )
   }
-
-  return (
-    <button
-      className={`usa-button usa-button--small display-block ${buttonStyle}`}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      <Trans i18nKey={labelKey} />
-    </button>
-  )
-}
+)
 
 export default Button
