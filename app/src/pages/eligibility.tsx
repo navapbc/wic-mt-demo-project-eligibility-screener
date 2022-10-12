4ab94a6bd00b6e83341fc2289cf261bd5664600f
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-12 02:06:39 -0700

    Fully refactor eligibilit page.
    - Drop useEffect(). Not needed
    - Use a more narrow requirements check
    - Add routing handling using next/router, not based on previous page
    - Use new data structure for user data
    - DRY up input choices
    - Write tests for all logic

--- app/src/pages/eligibility.tsx
+++ app/src/pages/eligibility.tsx
@@ -1,77 +1,122 @@
+import type { EligibilityData, ModifySessionProps } from '@customTypes/common'
 import type { GetServerSideProps, NextPage } from 'next'
 import { Trans } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { ChangeEvent, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { ChangeEvent, useState } from 'react'
 
 import BackLink from '@components/BackLink'
 import ButtonLink from '@components/ButtonLink'
 import InputChoiceGroup from '@components/InputChoiceGroup'
 import RequiredQuestionStatement from '@components/RequiredQuestionStatement'
 
-import type { ModifySessionProps } from '../types/common'
+const Eligibility: NextPage<ModifySessionProps> = (
+  props: ModifySessionProps
+) => {
+  // Get the session from props.
+  const { session, setSession } = props
+  // Initialize form as a state with the value in session.
+  const [form, setForm] = useState<EligibilityData>(session.eligibility)
 
-interface EligibilityProps extends ModifySessionProps {
-  previousRoute: string
-}
-
-const Eligibility: NextPage<EligibilityProps> = (props: EligibilityProps) => {
-  const { session, setSession, previousRoute } = props
-  const incomeRoute = '/income'
-  const [continueBtn, setContinueBtn] = useState({
-    labelKey: 'continue',
-    route: incomeRoute,
-  })
-  const [form, setForm] = useState(session?.eligibility)
-  const requiredMet = () => {
-    const categorical = [
-      'pregnant',
-      'baby',
-      'child',
-      'guardian',
-      'pregnant',
-      'none',
-    ].some((category) => form[category as keyof typeof form])
-    const programs = ['insurance', 'snap', 'tanf', 'none2'].some(
-      (program) => form[program as keyof typeof form]
+  // Function to check whether all the required fields in this form
+  // page have been filled out.
+  // @TODO: This could be further refactored to be more generic.
+  const isRequiredMet = (formToCheck: EligibilityData) => {
+    return (
+      formToCheck.residential !== '' &&
+      formToCheck.categorical.length > 0 &&
+      formToCheck.previouslyEnrolled !== '' &&
+      formToCheck.adjunctive.length > 0
     )
-
-    return form.residential && categorical && form.before && programs
   }
-  const [disabled, setDisabled] = useState<boolean>(!requiredMet())
 
-  useEffect(() => {
-    /* NOTE: We are using useEffect() because we want to make sure the props provided by getServerSideProps() are reliably loaded into the page. */
-    const prevRouteIndex = props.previousRoute.lastIndexOf('/')
-    const previousRoute = props.previousRoute.substring(prevRouteIndex)
-    if (form.none) {
-      setContinueBtn({ ...continueBtn, route: '/other-benefits' })
-    } else if (previousRoute === '/review') {
-      setContinueBtn({
-        labelKey: 'updateAndReturn',
-        route: previousRoute,
-      })
-    } else setContinueBtn({ ...continueBtn, route: incomeRoute })
-  }, [form.none, props.previousRoute])
-
-  useEffect(() => {
-    setDisabled(!requiredMet())
-  }, [form])
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value, name }: { value: string; name: string } = e.target
-    const castValue = value as keyof typeof form
-    let newValue
-
-    if (['residential', 'before'].includes(name)) {
-      newValue = { [name]: value }
+  // Function to update button route.
+  // Route the user to /other-benefits if they do not meet basic eligibility requirements.
+  // Route the user to /income if they do not meet adjunctive eligibility requirements.
+  // Otherwise, route the user to /choose-clinic.
+  const getRouting = (formToCheck: EligibilityData) => {
+    const isResident = formToCheck.residential === 'yes'
+    const hasCategory = !formToCheck.categorical.includes('none')
+    if (!isResident || !hasCategory) {
+      return '/other-benefits'
+    } else if (formToCheck.adjunctive.includes('none')) {
+      return '/income'
     } else {
-      newValue = { [castValue]: !form[castValue] }
+      return '/choose-clinic'
+    }
+  }
+
+  // Set up action button and routing.
+  const defaultActionButton = {
+    labelKey: 'continue',
+    route: getRouting(form),
+  }
+  const reviewActionButton = {
+    labelKey: 'updateAndReturn',
+    route: '/review',
+  }
+  // Set up routing to determine if the user is reviewing previously entered data.
+  const router = useRouter()
+  // If the user is reviewing previously entered data, use the review button.
+  // Otherwise, use the default button.
+  const [continueBtn, setContinueBtn] = useState(
+    router.query.mode === 'review' ? reviewActionButton : defaultActionButton
+  )
+
+  // Set a state for whether the form requirements have been met and the
+  // form can be submitted. Otherwise, disable the submit button.
+  const [disabled, setDisabled] = useState(!isRequiredMet(form))
+
+  // Handle all form element changes.
+  // - Determine new form values
+  // - Update the form and session states
+  // - Update whether the action button is disabled based on whether all
+  //   required fields have been filled out
+  // - Update the action button and routing for which page the user should be
+  //   routed to based on user-entered data
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name, type }: { value: string; name: string; type: string } =
+      e.target
+    let newForm
+
+    // Handle radio buttons and checkboxes need to be handled differently.
+    // Radio buttons are a boolean setting, so override the existing value.
+    if (type === 'radio') {
+      newForm = { ...form, [name]: value }
+    }
+    // Checkboxes are an array because multiple options are accepted,
+    // so manage the array elements.
+    else if (type === 'checkbox') {
+      // Cast the name as a key in form.
+      const castName = name as 'categorical' | 'adjunctive'
+      // If the checkbox is checked and that checkbox value isn't in the array,
+      // add it to the array.
+      if (e.target.checked && !form[castName].includes(value)) {
+        const checkboxArray = [...form[castName]]
+        checkboxArray.push(value)
+        newForm = { ...form, [castName]: checkboxArray }
+      }
+      // If the checkbox is unchecked and the checkbox value IS in the array,
+      // remove it from the array.
+      else if (!e.target.checked && form[castName].includes(value)) {
+        const checkboxArray = form[castName].filter((element) => {
+          return element !== value
+        })
+        newForm = { ...form, [castName]: checkboxArray }
+      }
     }
 
-    const newForm = { ...form, ...newValue }
+    // Update state if there are changes to update.
+    if (newForm) {
+      setForm(newForm)
+      setSession({ ...session, eligibility: newForm })
 
-    setForm(newForm)
-    setSession({ ...session, eligibility: newForm })
+      // Handle button routing.
+      setContinueBtn({ ...continueBtn, route: getRouting(newForm) })
+
+      // Update whether the submit button is enabled or disabled.
+      setDisabled(!isRequiredMet(newForm))
+    }
   }
 
   return (
@@ -86,22 +131,15 @@ const Eligibility: NextPage<EligibilityProps> = (props: EligibilityProps) => {
           required
           titleKey="Eligibility.residential"
           type="radio"
-          choices={[
-            {
-              checked: form.residential === 'yes',
-              handleChange,
-              labelKey: 'Eligibility.yes',
+          choices={['yes', 'no'].map((option) => {
+            return {
+              checked: form.residential === option,
+              handleChange: handleChange,
+              labelKey: `Eligibility.${option}`,
               name: 'residential',
-              value: 'yes',
-            },
-            {
-              checked: form.residential === 'no',
-              handleChange,
-              labelKey: 'Eligibility.no',
-              name: 'residential',
-              value: 'no',
-            },
-          ]}
+              value: option,
+            }
+          })}
         />
         <InputChoiceGroup
           accordion={{
@@ -112,101 +150,51 @@ const Eligibility: NextPage<EligibilityProps> = (props: EligibilityProps) => {
           titleKey="Eligibility.categorical"
           type="checkbox"
           choices={[
-            {
-              checked: form.pregnant,
-              handleChange,
-              labelKey: 'Eligibility.pregnant',
-              value: 'pregnant',
-            },
-            {
-              checked: form.baby,
-              handleChange,
-              labelKey: 'Eligibility.baby',
-              value: 'baby',
-            },
-            {
-              checked: form.child,
-              handleChange,
-              labelKey: 'Eligibility.child',
-              value: 'child',
-            },
-            {
-              checked: form.guardian,
-              handleChange,
-              labelKey: 'Eligibility.guardian',
-              value: 'guardian',
-            },
-            {
-              checked: form.loss,
-              handleChange,
-              labelKey: 'Eligibility.loss',
-              value: 'loss',
-            },
-            {
-              checked: form.none,
-              handleChange,
-              labelKey: 'Eligibility.none',
-              value: 'none',
-            },
-          ]}
+            'pregnant',
+            'baby',
+            'child',
+            'guardian',
+            'loss',
+            'none',
+          ].map((option) => {
+            return {
+              checked: form.categorical.includes(option),
+              handleChange: handleChange,
+              labelKey: `Eligibility.${option}`,
+              name: 'categorical',
+              value: option,
+            }
+          })}
         />
         <InputChoiceGroup
           required
-          titleKey="Eligibility.before"
+          titleKey="Eligibility.previouslyEnrolled"
           type="radio"
-          choices={[
-            {
-              checked: form.before === 'yes2',
-              handleChange,
-              labelKey: 'Eligibility.yes',
-              name: 'before',
-              value: 'yes2' /* TODO: refactor */,
-            },
-            {
-              checked: form.before === 'no2',
-              handleChange,
-              labelKey: 'Eligibility.no',
-              name: 'before',
-              value: 'no2',
-            },
-          ]}
+          choices={['yes', 'no'].map((option) => {
+            return {
+              checked: form.previouslyEnrolled === option,
+              handleChange: handleChange,
+              labelKey: `Eligibility.${option}`,
+              name: 'previouslyEnrolled',
+              value: option,
+            }
+          })}
         />
         <InputChoiceGroup
           required
           titleKey="Eligibility.programs"
           type="checkbox"
-          choices={[
-            {
-              checked: form.insurance,
-              handleChange,
-              labelKey: 'Eligibility.insurance',
-              value: 'insurance',
-            },
-            {
-              checked: form.snap,
-              handleChange,
-              labelKey: 'Eligibility.snap',
-              value: 'snap',
-            },
-            {
-              checked: form.tanf,
-              handleChange,
-              labelKey: 'Eligibility.tanf',
-              value: 'tanf',
-            },
-            {
-              checked: form.fdpir,
-              handleChange,
-              labelKey: 'Eligibility.fdpir',
-              value: 'fdpir',
-            },
-            {
-              checked: form.none2,
-              handleChange,
-              labelKey: 'Eligibility.none',
-              value: 'none2',
-            },
-          ]}
+          choices={['insurance', 'snap', 'tanf', 'fdpir', 'none'].map(
+            (option) => {
+              return {
+                checked: form.adjunctive.includes(option),
+                handleChange: handleChange,
+                labelKey: `Eligibility.${option}`,
+                name: 'adjunctive',
+                value: option,
+              }
+            }
+          )}
         />
         <ButtonLink
           href={continueBtn.route}
@@ -218,13 +206,9 @@ const Eligibility: NextPage<EligibilityProps> = (props: EligibilityProps) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  locale,
-  req,
-}) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
-      previousRoute: req.headers.referer,
       ...(await serverSideTranslations(locale || 'en', ['common'])),
     },
   }
--- app/tests/pages/eligibility.test.tsx
+++ app/tests/pages/eligibility.test.tsx
@@ -1,58 +1,295 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import type { EligibilityData, SessionData } from '@customTypes/common'
+import { act, fireEvent, render, screen } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks'
 import { axe } from 'jest-axe'
+import cloneDeep from 'lodash/cloneDeep'
+import mockRouter from 'next-router-mock'
+import singletonRouter, { useRouter } from 'next/router'
+import { useState } from 'react'
+import renderer from 'react-test-renderer'
 
 import Eligibility from '@pages/eligibility'
 
+/**
+ * Test setup
+ */
+
+// Mock the nextjs router
+jest.mock('next/router', () => require('next-router-mock'))
+// This is needed for mocking 'next/link':
+jest.mock('next/dist/client/router', () => require('next-router-mock'))
+// Temporary workaround for all next.js 12.2.0+
+// See active issue: https://github.com/scottrippey/next-router-mock/issues/58
+jest.mock('next/dist/shared/lib/router-context', () => {
+  const { createContext } = require('react')
+  const router = require('next-router-mock').default
+  const RouterContext = createContext(router)
+  return { RouterContext }
+})
+
+// Mock the session
+let mockSession: SessionData
+const emptyMockEligibility: EligibilityData = {
+  residential: '',
+  categorical: [],
+  previouslyEnrolled: '',
+  adjunctive: [],
+}
+const emptyMockSession: SessionData = {
+  clinic: undefined,
+  contact: {
+    firstName: '',
+    lastName: '',
+    phone: '',
+    comments: '',
+  },
+  eligibility: cloneDeep(emptyMockEligibility),
+}
+const setMockSession = jest.fn()
+
+/**
+ * Begin tests
+ */
+
 describe('Eligibility', () => {
-  it('should render first question text', () => {
-    render(<Eligibility previousRoute="/review" />)
+  beforeEach(() => {
+    // Set the router before each test.
+    act(() => {
+      mockRouter.setCurrentUrl('/eligibility')
+    })
 
-    const question = screen.getByText(/Do you live or work in Montana?/i)
+    // Reset the mock session before each test.
+    mockSession = cloneDeep(emptyMockSession)
+  })
 
-    expect(question).toBeInTheDocument()
-    expect(question).toMatchSnapshot()
+  it('should match full page snapshot', () => {
+    const tree = renderer
+      .create(<Eligibility session={mockSession} setSession={setMockSession} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   it('should pass accessibility scan', async () => {
-    const { container } = render(<Eligibility previousRoute="/review" />)
-    const results = await axe(container)
+    const { container } = render(
+      <Eligibility session={mockSession} setSession={setMockSession} />
+    )
+
+    // Must call axe() like this to satisfy react testing.
+    let results
+    await act(async () => {
+      results = await axe(container)
+    })
 
     expect(results).toHaveNoViolations()
   })
 
-  describe('residential radios', () => {
-    it('should select yes option', () => {
-      render(<Eligibility previousRoute="/review" />)
+  describe('Action button initial behavior', () => {
+    const combinations = [
+      ['no values are set', '', [], '', []],
+      ['residential is not set', '', ['anything'], 'anything', ['anything']],
+      ['categorical is not set', 'anything', [], 'anything', ['anything']],
+      [
+        'previouslyEnrolled is not set',
+        'anything',
+        ['anything'],
+        '',
+        ['anything'],
+      ],
+      ['adjunctive is not set', 'anything', ['anything'], 'anything', []],
+    ]
+    it.each(combinations)(
+      'should be disabled if %s',
+      (
+        description,
+        residential,
+        categorical,
+        previouslyEnrolled,
+        adjunctive
+      ) => {
+        // Set the initial mock session values
+        mockSession.eligibility.residential = residential as string
+        mockSession.eligibility.categorical = categorical as string[]
+        mockSession.eligibility.previouslyEnrolled =
+          previouslyEnrolled as string
+        mockSession.eligibility.adjunctive = adjunctive as string[]
+        render(
+          <Eligibility session={mockSession} setSession={setMockSession} />
+        )
 
-      const yesRadio = screen.getAllByLabelText(/Yes/i)[0]
+        // Check the button is disabled.
+        const button = screen.getByRole('button', { name: /Continue/i })
+        expect(button).toBeDisabled()
+      }
+    )
 
-      expect(yesRadio).not.toBeChecked()
-      fireEvent.click(yesRadio)
-      expect(yesRadio).toBeChecked()
+    it('should be enabled if all requirements are met', () => {
+      // Set the initial mock session values
+      mockSession.eligibility.residential = 'anything'
+      mockSession.eligibility.categorical = ['anything']
+      mockSession.eligibility.previouslyEnrolled = 'anything'
+      mockSession.eligibility.adjunctive = ['anything']
+      render(<Eligibility session={mockSession} setSession={setMockSession} />)
+
+      // Check the button is enabled.
+      const button = screen.getByRole('button', { name: /Continue/i })
+      expect(button).not.toBeDisabled()
     })
   })
 
-  describe('continue link', () => {
-    it.skip('should link to income page by default', () => {
-      render(<Eligibility previousRoute="/" />)
+  describe('Action button interactive behavior', () => {
+    it('should stay disabled until all requirements are met and re-disable if reqirements are unmet', () => {
+      render(<Eligibility session={mockSession} setSession={setMockSession} />)
+      const button = screen.getByRole('button', { name: /Continue/i })
+      expect(button).toBeDisabled()
 
-      const continueBtn = screen.getByText(/Continue/i)
+      // Select a residental option.
+      const residential = screen.getAllByRole('radio', { name: /Yes/i })[0]
+      expect(residential).not.toBeChecked()
+      fireEvent.click(residential)
+      expect(residential).toBeChecked()
+      expect(button).toBeDisabled()
 
-      expect(continueBtn.getAttribute('href')).toBe('/income')
+      // Select a categorical option.
+      const categorical = screen.getByRole('checkbox', {
+        name: /I'm pregnant/i,
+      })
+      expect(categorical).not.toBeChecked()
+      fireEvent.click(categorical)
+      expect(categorical).toBeChecked()
+      expect(button).toBeDisabled()
+
+      // Select a previouslyEnrolled option.
+      const previouslyEnrolled = screen.getAllByRole('radio', {
+        name: /Yes/i,
+      })[1]
+      expect(previouslyEnrolled).not.toBeChecked()
+      fireEvent.click(previouslyEnrolled)
+      expect(previouslyEnrolled).toBeChecked()
+      expect(button).toBeDisabled()
+
+      // Select a adjunctive option.
+      const adjunctive = screen.getByRole('checkbox', { name: /FDPIR/i })
+      expect(adjunctive).not.toBeChecked()
+      fireEvent.click(adjunctive)
+      expect(adjunctive).toBeChecked()
+      // The button should finally be enabled.
+      expect(button).not.toBeDisabled()
+
+      // De-select a categorical option.
+      fireEvent.click(categorical)
+      expect(categorical).not.toBeChecked()
+      // The button should not be disabled again.
+      expect(button).toBeDisabled()
+
+      // Re-select a categorical option.
+      fireEvent.click(categorical)
+      expect(categorical).toBeChecked()
+      expect(button).not.toBeDisabled()
+
+      // De-select a adjunctive option.
+      fireEvent.click(adjunctive)
+      expect(adjunctive).not.toBeChecked()
+      // The button should not be disabled again.
+      expect(button).toBeDisabled()
+
+      // Re-select a adjunctive option.
+      fireEvent.click(adjunctive)
+      expect(adjunctive).toBeChecked()
+      expect(button).not.toBeDisabled()
+    })
+  })
+
+  describe('Action button review behavior', () => {
+    it('should render differently in review mode', () => {
+      // Set the path to review mode.
+      act(() => {
+        mockRouter.setCurrentUrl('/eligibility?mode=review')
+      })
+      render(<Eligibility session={mockSession} setSession={setMockSession} />)
+      const button = screen.getByRole('button', { name: /Update/i })
+      expect(button).toBeInTheDocument()
+    })
+  })
+})
+
+describe('Eligibility routing', () => {
+  it('should route to /other-benefits by default', () => {
+    act(() => {
+      mockRouter.setCurrentUrl('/eligibility')
+    })
+    const filledSession = cloneDeep(mockSession)
+    filledSession.eligibility = {
+      residential: 'anything',
+      categorical: ['anything'],
+      previouslyEnrolled: 'anything',
+      adjunctive: ['anything'],
+    }
+    render(<Eligibility session={filledSession} setSession={setMockSession} />)
+    const button = screen.getByRole('button', { name: /Continue/i })
+    act(() => {
+      fireEvent.click(button)
     })
 
-    it.skip('should link to other benefits page if none is selected for question 2', () => {
-      render(<Eligibility previousRoute="/" />)
+    expect(singletonRouter).toMatchObject({ asPath: '/other-benefits' })
+  })
 
-      const noneBtn = screen.getAllByLabelText(/None of the above/i)[0]
-
-      expect(noneBtn).not.toBeChecked()
-      fireEvent.click(noneBtn)
-      expect(noneBtn).toBeChecked()
-
-      const continueBtn = screen.getByText(/Continue/i)
-
-      expect(continueBtn.getAttribute('href')).toBe('/other-benefits')
+  it('should route to /other-benefits if categorical includes none', () => {
+    act(() => {
+      mockRouter.setCurrentUrl('/eligibility')
     })
+    const filledSession = cloneDeep(mockSession)
+    filledSession.eligibility = {
+      residential: 'yes',
+      categorical: ['anything', 'none'],
+      previouslyEnrolled: 'anything',
+      adjunctive: ['anything'],
+    }
+    render(<Eligibility session={filledSession} setSession={setMockSession} />)
+    const button = screen.getByRole('button', { name: /Continue/i })
+    act(() => {
+      fireEvent.click(button)
+    })
+
+    expect(singletonRouter).toMatchObject({ asPath: '/other-benefits' })
+  })
+
+  it('should route to /income if adjunctive includes none', () => {
+    act(() => {
+      mockRouter.setCurrentUrl('/eligibility')
+    })
+    const filledSession = cloneDeep(mockSession)
+    filledSession.eligibility = {
+      residential: 'yes',
+      categorical: ['anything'],
+      previouslyEnrolled: 'anything',
+      adjunctive: ['anything', 'none'],
+    }
+    render(<Eligibility session={filledSession} setSession={setMockSession} />)
+    const button = screen.getByRole('button', { name: /Continue/i })
+    act(() => {
+      fireEvent.click(button)
+    })
+
+    expect(singletonRouter).toMatchObject({ asPath: '/income' })
+  })
+
+  it('should route to /choose-clinic if adjunctive qualifies', () => {
+    act(() => {
+      mockRouter.setCurrentUrl('/eligibility')
+    })
+    const filledSession = cloneDeep(mockSession)
+    filledSession.eligibility = {
+      residential: 'yes',
+      categorical: ['anything'],
+      previouslyEnrolled: 'anything',
+      adjunctive: ['anything'],
+    }
+    render(<Eligibility session={filledSession} setSession={setMockSession} />)
+    const button = screen.getByRole('button', { name: /Continue/i })
+    act(() => {
+      fireEvent.click(button)
+    })
+
+    expect(singletonRouter).toMatchObject({ asPath: '/choose-clinic' })
   })
 })
