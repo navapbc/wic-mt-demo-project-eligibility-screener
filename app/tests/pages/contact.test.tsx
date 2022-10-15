fca0d4d97eb95c103e671809389df518bbdb16b0
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-14 20:26:27 -0700

    Contact: update tests.

--- app/tests/pages/contact.test.tsx
+++ app/tests/pages/contact.test.tsx
@@ -1,50 +1,202 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import type { SessionData } from '@customTypes/common'
+import { act, fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { axe } from 'jest-axe'
+import cloneDeep from 'lodash/cloneDeep'
+import mockRouter from 'next-router-mock'
+import singletonRouter from 'next/router'
+import renderer from 'react-test-renderer'
 
 import Contact from '@pages/contact'
 
-describe('Contact', () => {
-  it('should render first name prompt', () => {
-    render(<Contact previousRoute="/review" />)
+import { initialSessionData } from '@utils/sessionData'
 
-    const prompt = screen.getByText(/First name/i)
+/**
+ * Test setup
+ */
 
-    expect(prompt).toBeInTheDocument()
-    expect(prompt).toMatchSnapshot()
+// Mock the session
+let mockSession: SessionData = initialSessionData
+export const setMockSession = jest.fn()
+
+// Set the router to the default page path.
+function setupDefaultRoute(): void {
+  act(() => {
+    mockRouter.setCurrentUrl('/contact')
+  })
+}
+
+// Setup function using AHA principle.
+// See https://kentcdodds.com/blog/avoid-nesting-when-youre-testing#apply-aha-avoid-hasty-abstractions
+// Return type taken from
+// https://github.com/testing-library/user-event/pull/983#issuecomment-1185537044
+function setup(): ReturnType<typeof userEvent['setup']> {
+  setupDefaultRoute()
+
+  // Reset the mock session before each test.
+  mockSession = cloneDeep(initialSessionData)
+
+  // Set up userEvent
+  // See https://testing-library.com/docs/user-event/intro#writing-tests-with-userevent
+  const user = userEvent.setup()
+
+  return user
+}
+
+/**
+ * Begin tests
+ */
+
+it('should match full page snapshot', () => {
+  setup()
+  const tree = renderer
+    .create(<Contact session={mockSession} setSession={setMockSession} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('should pass accessibility scan', async () => {
+  setup()
+  const { container } = render(
+    <Contact session={mockSession} setSession={setMockSession} />
+  )
+
+  // Must call axe() like this to satisfy react testing.
+  let results
+  await act(async () => {
+    results = await axe(container)
   })
 
-  it('should update name input value', () => {
-    render(<Contact previousRoute="/review" />)
+  expect(results).toHaveNoViolations()
+})
 
-    const nameInput = screen.getByLabelText(/First name/i) as HTMLInputElement
+const combinations = [
+  ['no values are set', '', '', ''],
+  ['firstName is not set', '', 'anything', 'anything'],
+  ['lastName is not set', 'anything', '', 'anything'],
+  ['phone is not set', 'anything', 'anything', ''],
+]
+it.each(combinations)(
+  'action button should be disabled if %s',
+  (description, firstName, lastName, phone) => {
+    setup()
+    // Set the initial mock session values
+    mockSession.contact.firstName = firstName
+    mockSession.contact.lastName = lastName
+    mockSession.contact.phone = phone
+    render(<Contact session={mockSession} setSession={setMockSession} />)
 
-    expect(nameInput.value).toBe('')
-    fireEvent.change(nameInput, { target: { value: 'Jane' } })
-    expect(nameInput.value).toBe('Jane')
+    // Check the button is disabled.
+    const button = screen.getByRole('button', { name: /Continue/i })
+    expect(button).toBeDisabled()
+  }
+)
+
+it('number input should not allow non-numbers', async () => {
+  const user = setup()
+  render(<Contact session={mockSession} setSession={setMockSession} />)
+
+  const phone = screen.getByRole('textbox', { name: /Phone/ })
+  expect(phone).toHaveValue('')
+  await user.type(phone, 'not a number')
+  expect(phone).toHaveValue('')
+})
+
+it('number input should format number', async () => {
+  const user = setup()
+  render(<Contact session={mockSession} setSession={setMockSession} />)
+
+  const phone = screen.getByRole('textbox', { name: /Phone/ })
+  expect(phone).toHaveValue('')
+  await user.type(phone, '1231231234')
+  expect(phone).toHaveValue('123-123-1234')
+})
+
+it('action button should be enabled if all requirements are met', () => {
+  setup()
+  // Set the initial mock session values
+  mockSession.contact.firstName = 'anything'
+  mockSession.contact.lastName = 'anything'
+  mockSession.contact.phone = '1231231234'
+  render(<Contact session={mockSession} setSession={setMockSession} />)
+
+  // Check the button is enabled.
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).not.toBeDisabled()
+})
+
+it('action button should stay disabled until all requirements are met and re-disable if reqirements are unmet', async () => {
+  const user = setup()
+
+  render(<Contact session={mockSession} setSession={setMockSession} />)
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).toBeDisabled()
+
+  // Enter a first name.
+  const firstName = screen.getByRole('textbox', { name: /First/ })
+  expect(firstName).toHaveValue('')
+  await user.type(firstName, 'Jack')
+  expect(firstName).toHaveValue('Jack')
+  expect(button).toBeDisabled()
+
+  // Enter a last name.
+  const lastName = screen.getByRole('textbox', { name: /Last/ })
+  expect(lastName).toHaveValue('')
+  await user.type(lastName, 'O Lantern')
+  expect(lastName).toHaveValue('O Lantern')
+  expect(button).toBeDisabled()
+
+  // Enter a phone number.
+  const phone = screen.getByRole('textbox', { name: /Phone/ })
+  expect(phone).toHaveValue('')
+  await user.type(phone, '1231231234')
+  expect(phone).toHaveValue('123-123-1234')
+  // Action button should finally not be disabled.
+  expect(button).not.toBeDisabled()
+
+  // Clear a text field.
+  // The button should be disabled.
+  await user.clear(firstName)
+  expect(firstName).toHaveValue('')
+  expect(button).toBeDisabled()
+
+  // Filling the comments field should not impact the button.
+  const comments = screen.getByRole('textbox', { name: /Comments/ })
+  expect(comments).toHaveValue('')
+  await user.type(comments, 'I wish to submit a comment')
+  expect(comments).toHaveValue('I wish to submit a comment')
+  expect(button).toBeDisabled()
+
+  // Fill the text field.
+  // The button should be enabled.
+  await user.type(firstName, 'Jacques')
+  expect(firstName).toHaveValue('Jacques')
+  expect(button).not.toBeDisabled()
+})
+
+it('action button should render differently in review mode', () => {
+  // Set the path to review mode.
+  act(() => {
+    mockRouter.setCurrentUrl('/contact?mode=review')
   })
+  mockSession = cloneDeep(initialSessionData)
+  render(<Contact session={mockSession} setSession={setMockSession} />)
+  const button = screen.getByRole('button', { name: /Update/i })
+  expect(button).toBeInTheDocument()
+})
 
-  describe('phone number input', () => {
-    it('should not allow non-numbers', () => {
-      render(<Contact previousRoute="/review" />)
+it('should route to /review', async () => {
+  const user = setup()
+  const filledSession = cloneDeep(mockSession)
+  filledSession.contact = {
+    firstName: 'Jack',
+    lastName: 'O Lantern',
+    phone: '123-123-1234',
+    comments: '',
+  }
+  render(<Contact session={filledSession} setSession={setMockSession} />)
+  const button = screen.getByRole('button', { name: /Continue/i })
+  await user.click(button)
 
-      const phoneNumber = screen.getByLabelText(
-        /Phone number/i
-      ) as HTMLInputElement
-
-      expect(phoneNumber.value).toBe('')
-      fireEvent.change(phoneNumber, { target: { value: 'not a number' } })
-      expect(phoneNumber.value).toBe('')
-    })
-
-    it('should format number', () => {
-      render(<Contact previousRoute="/review" />)
-
-      const phoneNumber = screen.getByLabelText(
-        /Phone number/i
-      ) as HTMLInputElement
-
-      expect(phoneNumber.value).toBe('')
-      fireEvent.change(phoneNumber, { target: { value: '1111111111' } })
-      expect(phoneNumber.value).toBe('111-111-1111')
-    })
-  })
+  expect(singletonRouter).toMatchObject({ asPath: '/review' })
 })
