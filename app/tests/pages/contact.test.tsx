ab8820bd5d875e2fbf55f3e7dc4b5910c119fc4f
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-14 23:54:14 -0700

    Remove unused import in contact.test.tsx.

--- app/tests/pages/contact.test.tsx
+++ app/tests/pages/contact.test.tsx
@@ -1,5 +1,5 @@
 import type { SessionData } from '@customTypes/common'
-import { act, fireEvent, render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { axe } from 'jest-axe'
 import cloneDeep from 'lodash/cloneDeep'
