b788ec3b0a537e6e4f5b1be812effd1aff651c7e
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-21 15:26:47 -0700

    Routing: first attempt. put logic in _app.tsx.

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 
 import Layout from '@components/Layout'
+import RoutingError from '@components/RoutingError'
 
 import useSessionStorage from '@src/hooks/useSessionStorage'
 import '@styles/styles.scss'
-import { getBackRoute } from '@utils/routing'
+import { getBackRoute, hasRoutingIssues } from '@utils/routing'
 import { initialSessionData } from '@utils/sessionData'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -20,16 +21,33 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const reviewMode = router.query.mode === 'review'
 
+  // Handle back links.
   let backRoute = '/'
   try {
     backRoute = getBackRoute(router.pathname, session)
   } catch (e: unknown) {
     const error = e as Error
     console.log(`error caught: ${error.message}`)
-    // @TODO: Something bad happened on the /choose-clinic page with regard to session. Route to '/' with an error message
+    // Something bad happened on the /choose-clinic page with regard to session. Route to '/' with an error message.
+    router.push({ pathname: '/', query: { error: 'missing-data' } })
   }
 
-  // @TODO: all pages need form validation before loading, re-routing, and error handling
+  // Handle form wizard page access.
+  try {
+    const outcome = hasRoutingIssues(router, session)
+    if (outcome.error) {
+      // See https://nextjs.org/docs/api-reference/next/router#potential-solutions
+      // disable the linting on the next line
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      router.push({ pathname: outcome.cause, query: { error: 'missing-data' } })
+    }
+  } catch (e: unknown) {
+    const error = e as Error
+    console.log(`error caught: ${error.message}`)
+    // Something bad happened with regard to session. Route to '/' with an error message.
+    router.push({ pathname: '/', query: { error: 'missing-data' } })
+  }
+
   // @TODO: fix conditional routing for /eligibility page with regard to review
   // @TODO: add tests for components
   const props = {
@@ -43,7 +61,10 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
-      <Component {...props} />
+      <>
+        {router.query.error === 'missing-data' && <RoutingError />}
+        <Component {...props} />
+      </>
     </Layout>
   )
 }
