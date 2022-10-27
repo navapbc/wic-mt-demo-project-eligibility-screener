acbea9f0130d9ec99da7f473bfb7615d875ef1d2
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-27 12:01:13 -0700

    Confirmation: update page, tests, stories.

--- app/stories/pages/confirmation.stories.tsx
+++ app/stories/pages/confirmation.stories.tsx
@@ -1,14 +1,35 @@
+import { useState } from '@storybook/client-api'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import ConfirmationPage from '@pages/confirmation'
 
+import { getMockSessionData } from '../../tests/helpers/mockData'
+
 export default {
   title: 'Pages/Confirmation',
   component: ConfirmationPage,
+  argTypes: {
+    session: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as ComponentMeta<typeof ConfirmationPage>
 
-const Template: ComponentStory<typeof ConfirmationPage> = () => (
-  <ConfirmationPage />
-)
+const Template: ComponentStory<typeof ConfirmationPage> = (args) => {
+  const [session, setSession] = useState(args.session)
+  args.session = session
+  args.setSession = setSession
+  return <ConfirmationPage {...args} />
+}
 
 export const Confirmation = Template.bind({})
+Confirmation.args = {
+  session: getMockSessionData(),
+}
+Confirmation.parameters = {
+  nextRouter: {
+    path: '/confirmation',
+  },
+}
