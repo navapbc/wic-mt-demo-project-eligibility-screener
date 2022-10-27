b7354b961391722fda362d96d7f136656fedd193
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-27 11:45:45 -0700

    ChooseClinic: update page, tests, stories.
    - Break ClinicSelectionList out of /choose-clinic

---
+++ app/src/components/ClinicSelectionList.tsx
@@ -0,0 +1,107 @@
+import { Trans } from 'next-i18next'
+import {
+  ChangeEvent,
+  Dispatch,
+  MouseEvent,
+  ReactElement,
+  SetStateAction,
+} from 'react'
+
+import Button from '@components/Button'
+import ButtonLink, { ButtonLinkProps } from '@components/ButtonLink'
+import ClinicInfo from '@components/ClinicInfo'
+import Required from '@components/Required'
+
+import { Clinic } from '@src/types'
+
+export type ClinicSelectionListProps = {
+  filteredClinics: (Clinic | undefined)[]
+  expandList: boolean
+  setExpandList: Dispatch<SetStateAction<boolean>> | (() => unknown)
+  selectedClinic: Clinic | undefined
+  handleSelection: (e: ChangeEvent<HTMLInputElement>) => void
+  disabled: boolean
+  actionButton: ButtonLinkProps
+}
+
+const ClinicSelectionList = (props: ClinicSelectionListProps): ReactElement => {
+  const {
+    filteredClinics,
+    expandList,
+    setExpandList,
+    selectedClinic,
+    handleSelection,
+    disabled,
+    actionButton,
+  } = props
+
+  const unexpandedNumberClinics = 4
+
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    setExpandList(true)
+  }
+
+  let list: ReactElement = <></>
+  if (filteredClinics.length > 0) {
+    list = (
+      <>
+        <h2>
+          <Trans i18nKey="ChooseClinic.listTitle" />
+          <Required />
+        </h2>
+        <form className="usa-form usa-form--large">
+          <fieldset className="usa-fieldset">
+            {filteredClinics
+              ?.slice(
+                0,
+                expandList ? filteredClinics.length : unexpandedNumberClinics
+              )
+              .map(
+                (clinic, index) =>
+                  clinic && (
+                    <div className="usa-radio" key={index}>
+                      <input
+                        checked={selectedClinic?.clinic === clinic.clinic}
+                        className="usa-radio__input usa-radio__input--tile"
+                        id={clinic.clinic}
+                        onChange={handleSelection}
+                        type="radio"
+                        value={clinic.clinic}
+                      />
+                      <label
+                        className="usa-radio__label"
+                        htmlFor={clinic.clinic}
+                      >
+                        <ClinicInfo
+                          name={clinic.clinic}
+                          streetAddress={clinic.clinicAddress}
+                          phone={clinic.clinicTelephone}
+                          isFormElement={true}
+                        />
+                      </label>
+                    </div>
+                  )
+              )}
+            {filteredClinics.length > 1 && !expandList && (
+              <Button
+                labelKey="ChooseClinic.showMoreOptions"
+                style="unstyled"
+                onClick={handleClick}
+              />
+            )}
+          </fieldset>
+          <ButtonLink
+            disabled={disabled}
+            href={actionButton.href}
+            labelKey={actionButton.labelKey}
+          />
+        </form>
+      </>
+    )
+  }
+
+  return list
+}
+
+export default ClinicSelectionList
--- app/tests/pages/choose-clinic.test.tsx
+++ app/tests/pages/choose-clinic.test.tsx
@@ -1,25 +1,461 @@
-// test/pages/index.test.js
-import { render, screen, waitFor } from '@testing-library/react'
-import { axe } from 'jest-axe'
+import { render, screen } from '@testing-library/react'
+import singletonRouter from 'next/router'
 
 import ChooseClinic from '@pages/choose-clinic'
 
-describe('ChooseClinic', () => {
-  it('should render the heading', () => {
-    render(<ChooseClinic previousRoute="/review" />)
+import type { SessionData } from '@src/types'
 
-    const heading = screen.getByText(/Choose a clinic/i)
+import { setMockSession, setup } from '../helpers/setup'
+import {
+  getMockClinic,
+  mockInStateZipCode,
+  mockOutofStateZipCode,
+  setupClinicMocks,
+} from '../helpers/setupClinics'
+import {
+  testAccessibility,
+  testActionButtonReviewMode,
+  testActionButtonRoute,
+  testBackLink,
+  testSnapshot,
+} from '../helpers/sharedTests'
 
-    expect(heading).toBeInTheDocument()
-    expect(heading).toMatchSnapshot()
+/**
+ * Test setup
+ */
+
+setupClinicMocks()
+const route = '/choose-clinic'
+const backRoute = '/income'
+const forwardRoute = '/contact'
+const invalidZipCode = 'abcde'
+
+function setZipCode(mockSession: SessionData, type: string): SessionData {
+  if (type === 'inState') {
+    mockSession.chooseClinic.zipCode = mockInStateZipCode
+  } else if (type === 'outOfState') {
+    mockSession.chooseClinic.zipCode = mockOutofStateZipCode
+  } else if (type === 'invalid') {
+    mockSession.chooseClinic.zipCode = invalidZipCode
+  } else {
+    mockSession.chooseClinic.zipCode = ''
+  }
+  return mockSession
+}
+
+/**
+ * Begin tests
+ */
+
+it('should match full page snapshot', () => {
+  const { mockSession } = setup(route)
+  testSnapshot(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+})
+
+it('should pass accessibility scan', async () => {
+  const { mockSession } = setup(route)
+  await testAccessibility(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+})
+
+it('action button should render differently in review mode', () => {
+  let { mockSession } = setup(route)
+  mockSession = setZipCode(mockSession, 'inState')
+  mockSession.chooseClinic.clinic = getMockClinic()
+
+  testActionButtonReviewMode(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      reviewMode={true}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />,
+    route
+  )
+})
+
+it('should have a back link that matches the backRoute in default mode', () => {
+  const { mockSession } = setup(route)
+  testBackLink(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />,
+    backRoute
+  )
+})
+
+it('should have a back link that matches the backRoute in review mode', () => {
+  const { mockSession } = setup(route)
+  testBackLink(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      reviewMode={true}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />,
+    backRoute
+  )
+})
+
+it('should have an action button that routes to forwardRoute', async () => {
+  let { mockSession, user } = setup(route)
+  mockSession = setZipCode(mockSession, 'inState')
+  mockSession.chooseClinic.clinic = getMockClinic()
+  const element = (
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+  await testActionButtonRoute(element, forwardRoute, 'Continue', user)
+})
+
+it('action button should not render if no values are set', () => {
+  const { mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  // Check the button doesn't exist.
+  const button = screen.queryByRole('button', { name: /Continue/i })
+  expect(button).not.toBeInTheDocument()
+})
+
+it('action button should not render if only the zip code is set', () => {
+  let { mockSession } = setup(route)
+  mockSession = setZipCode(mockSession, 'inState')
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  // Check the button doesn't exist.
+  const button = screen.queryByRole('button', { name: /Continue/i })
+  expect(button).not.toBeInTheDocument()
+})
+
+it('action button should render if only the clinic is set', () => {
+  let { mockSession } = setup(route)
+  mockSession.chooseClinic.clinic = getMockClinic()
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  // Check the button should exist, but is disabled.
+  const button = screen.queryByRole('button', { name: /Continue/i })
+  expect(button).toBeInTheDocument()
+  expect(button).toBeDisabled()
+})
+
+it('action button should be disabled if the zip code is invalid', () => {
+  let { mockSession } = setup(route)
+  mockSession = setZipCode(mockSession, 'invalid')
+  mockSession.chooseClinic.clinic = getMockClinic()
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).toBeDisabled()
+})
+
+it('action button should render and be enabled if all requirements are met', () => {
+  let { mockSession } = setup(route)
+  mockSession = setZipCode(mockSession, 'inState')
+  mockSession.chooseClinic.clinic = getMockClinic()
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).not.toBeDisabled()
+})
+
+it('should display user values on refresh/page load', () => {
+  let { mockSession } = setup(route)
+  mockSession = setZipCode(mockSession, 'inState')
+  mockSession.chooseClinic.clinic = getMockClinic()
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  expect(zipCodeInput).toHaveValue(mockInStateZipCode)
+
+  const radioButtons = screen.getAllByRole('radio')
+  expect(radioButtons.length).toBe(1)
+  expect(radioButtons[0]).toBeChecked()
+})
+
+it('should display invalid zip code error if an invalid zip code is entered', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  let errorMessage = screen.queryByText(
+    /Please enter 5 digits for the ZIP code/
+  )
+  expect(errorMessage).not.toBeInTheDocument()
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, invalidZipCode)
+  expect(errorMessage).not.toBeInTheDocument()
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+  errorMessage = screen.queryByText(/Please enter 5 digits for the ZIP code/)
+  expect(errorMessage).toBeInTheDocument()
+})
+
+it('should display out of state zip code error if an out of state zip is entered', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  let errorMessage = screen.queryByText(
+    /Sorry, we could not find a match for this ZIP code/
+  )
+  expect(errorMessage).not.toBeInTheDocument()
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockOutofStateZipCode)
+  expect(errorMessage).not.toBeInTheDocument()
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+  errorMessage = screen.queryByText(
+    /Sorry, we could not find a match for this ZIP code/
+  )
+  expect(errorMessage).toBeInTheDocument()
+})
+
+it('should display a list of clinics if an in state zip is entered', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  const radioButtons = screen.getAllByRole('radio')
+  expect(radioButtons.length).toBeGreaterThan(0)
+  const showMoreButton = screen.getByRole('button', { name: /Show more/i })
+  expect(showMoreButton).not.toBeDisabled()
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).toBeDisabled()
+})
+
+it('should display expand the list if the button show more button is clicked', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  let radioButtons = screen.getAllByRole('radio')
+  const unexpandedNumberClinics = radioButtons.length
+
+  const showMoreButton = screen.getByRole('button', { name: /Show more/i })
+  expect(showMoreButton).not.toBeDisabled()
+  await user.click(showMoreButton)
+
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).toBeDisabled()
+
+  radioButtons = screen.getAllByRole('radio')
+  const expandedNumberClinics = radioButtons.length
+  expect(expandedNumberClinics).toBeGreaterThan(unexpandedNumberClinics)
+})
+
+it('should enable the action button when a clinic is selected from the unexpanded list', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  let clinic = screen.getByRole('radio', {
+    name: new RegExp(getMockClinic().clinic),
   })
+  expect(clinic).not.toBeChecked()
+  await user.click(clinic)
+  expect(clinic).toBeChecked()
 
-  it('should pass accessibility scan', async () => {
-    const { container } = render(<ChooseClinic previousRoute="/review" />)
-    const results = await axe(container)
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).not.toBeDisabled()
+})
 
-    await waitFor(() => {
-      expect(results).toHaveNoViolations()
-    })
+it('should enable the action button when a clinic is selected from the expanded list', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  const unexpandedClinics = screen.getAllByRole('radio')
+
+  const showMoreButton = screen.getByRole('button', { name: /Show more/i })
+  expect(showMoreButton).not.toBeDisabled()
+  await user.click(showMoreButton)
+
+  const expandedClinics = screen.getAllByRole('radio')
+  const newClinics = expandedClinics.filter(
+    (x) => !unexpandedClinics.includes(x)
+  )
+  await user.click(newClinics[0])
+
+  const button = screen.getByRole('button', { name: /Continue/i })
+  expect(button).not.toBeDisabled()
+})
+
+it('should remove the clinic list if the zip code field is modified', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  const radioButtons = screen.getAllByRole('radio')
+  expect(radioButtons.length).toBeGreaterThan(0)
+
+  await user.type(zipCodeInput, invalidZipCode)
+  const clinicsAfter = screen.queryByRole('radio')
+  expect(clinicsAfter).not.toBeInTheDocument()
+})
+
+it('should unselect the option if a new zip is searched', async () => {
+  const { user, mockSession } = setup(route)
+  render(
+    <ChooseClinic
+      session={mockSession}
+      setSession={setMockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+
+  const zipCodeInput = screen.getByRole('searchbox')
+  await user.type(zipCodeInput, mockInStateZipCode)
+
+  const searchButton = screen.getByRole('button', { name: /Search/i })
+  await user.click(searchButton)
+
+  const radioButtons = screen.getAllByRole('radio')
+  await user.click(radioButtons[0])
+
+  await user.click(searchButton)
+  const clinicsAfter = screen.getAllByRole('radio')
+  clinicsAfter.forEach((clinic) => {
+    expect(clinic).not.toBeChecked()
   })
 })
