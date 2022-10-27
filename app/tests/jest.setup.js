d2c47026861efe4b99542c0d8d20422691bed4f6
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-27 00:15:16 -0700

    Setup mock next/router for tests.

--- app/tests/jest.setup.js
+++ app/tests/jest.setup.js
@@ -1,4 +1,17 @@
+// Set up Axe
 require('@testing-library/jest-dom')
 const { toHaveNoViolations } = require('jest-axe')
-
 expect.extend(toHaveNoViolations)
+
+// Mock the nextjs router
+jest.mock('next/router', () => require('next-router-mock'))
+// This is needed for mocking 'next/link':
+jest.mock('next/dist/client/router', () => require('next-router-mock'))
+// Temporary workaround for all next.js 12.2.0+
+// See active issue: https://github.com/scottrippey/next-router-mock/issues/58
+jest.mock('next/dist/shared/lib/router-context', () => {
+  const { createContext } = require('react')
+  const router = require('next-router-mock').default
+  const RouterContext = createContext(router)
+  return { RouterContext }
+})
