4d74fe61b694459c21792ebb43f3b8ae03980dc9
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-27 01:26:32 -0700

    PageError: add new component.

---
+++ app/src/components/PageError.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from 'react'
+
+import Alert from '@components/Alert'
+
+import { i18nKey } from '@src/types'
+
+export type PageErrorProps = {
+  alertBody: i18nKey
+}
+
+export const PageError = (props: PageErrorProps): ReactElement => {
+  const { alertBody } = props
+  return (
+    <div className="margin-bottom-3">
+      <Alert alertBody={alertBody} type="error" icon={true} />
+    </div>
+  )
+}
+
+export default PageError
