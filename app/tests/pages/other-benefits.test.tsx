0c429b19dd85d628ab2fe926940ac0907ab58a36
Rocket ! <rocketnova@users.noreply.github.com>
2022-10-27 11:59:09 -0700

    OtherBenefits: update page, tests, stories.

--- app/tests/pages/other-benefits.test.tsx
+++ app/tests/pages/other-benefits.test.tsx
@@ -1,24 +1,87 @@
-import { render, screen, waitFor } from '@testing-library/react'
-import { axe } from 'jest-axe'
+import { render, screen } from '@testing-library/react'
+import singletonRouter from 'next/router'
 
 import OtherBenefits from '@pages/other-benefits'
 
-describe('OtherBenefits', () => {
-  it('should render the heading', () => {
-    render(<OtherBenefits />)
+import * as sessionModule from '@src/hooks/useSessionStorage'
+import { initialSessionData } from '@utils/sessionData'
 
-    const heading = screen.getByText(/Your eligibility/i)
+import { setMockSession, setup } from '../helpers/setup'
+import {
+  testAccessibility,
+  testBackLink,
+  testSnapshot,
+} from '../helpers/sharedTests'
 
-    expect(heading).toBeInTheDocument()
-    expect(heading).toMatchSnapshot()
-  })
+/**
+ * Test setup
+ */
 
-  it('should pass accessibility scan', async () => {
-    const { container } = render(<OtherBenefits />)
-    const results = await axe(container)
+const route = '/other-benefits'
+const backRoute = '/eligibility'
+const forwardRoute = '/'
 
-    await waitFor(() => {
-      expect(results).toHaveNoViolations()
-    })
-  })
+/**
+ * Begin tests
+ */
+
+it('should match full page snapshot', () => {
+  const { mockSession } = setup(route)
+  testSnapshot(
+    <OtherBenefits
+      sessionKey="mockSessionKey"
+      setSession={setMockSession}
+      session={mockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+})
+
+it('should pass accessibility scan', async () => {
+  const { mockSession } = setup(route)
+  await testAccessibility(
+    <OtherBenefits
+      sessionKey="mockSessionKey"
+      setSession={setMockSession}
+      session={mockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+})
+
+it('should have a back link that matches the backRoute', () => {
+  const { mockSession } = setup(route)
+  testBackLink(
+    <OtherBenefits
+      sessionKey="mockSessionKey"
+      setSession={setMockSession}
+      session={mockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />,
+    backRoute
+  )
+})
+
+it('should clear the session and redirect when the button is clicked', async () => {
+  const { mockSession, user } = setup(route)
+  // Spy on clearSessionStorage() to make sure it gets called.
+  const spy = jest.spyOn(sessionModule, 'clearSessionStorage')
+  render(
+    <OtherBenefits
+      sessionKey="mockSessionKey"
+      setSession={setMockSession}
+      session={mockSession}
+      backRoute={backRoute}
+      forwardRoute={forwardRoute}
+    />
+  )
+  const button = screen.getByRole('button', { name: /Return/i })
+  await user.click(button)
+
+  expect(setMockSession).toHaveBeenCalledWith(initialSessionData)
+  expect(singletonRouter).toMatchObject({ asPath: forwardRoute })
+  expect(spy).toHaveBeenCalled()
 })
