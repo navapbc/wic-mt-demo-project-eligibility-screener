247f223a6d969a3dd870b7c2de138d12e85919f5
Rocket ! <rocketnova@users.noreply.github.com>
2022-11-22 16:33:10 -0800

    Add component for page title.

---
+++ app/src/components/PageTitle.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from 'next-i18next'
+import Head from 'next/head'
+import { ReactElement } from 'react'
+
+export type PageTitleProps = {
+  pathname: string
+}
+
+const PageTitle = (props: PageTitleProps): ReactElement => {
+  const { pathname } = props
+  console.log(pathname)
+
+  const { t } = useTranslation('common')
+  let pageTitle = ''
+  let i18nPageKey = ''
+
+  // Handle the index page.
+  if (pathname === '/') {
+    i18nPageKey = 'Index'
+  }
+  // Convert all other pathnames to capital camel case to match i18n content file.
+  else {
+    i18nPageKey = pathname
+      .toLowerCase()
+      .replace(/([-_][a-z])/g, (group) =>
+        group.toUpperCase().replace('-', '').replace('_', '')
+      )
+      .replace(/^\/[a-z]/, (group) => group.replace('/', '').toUpperCase())
+  }
+
+  pageTitle = t(`${i18nPageKey}.title`)
+
+  // Fallback to the layout header if there is no page title found.
+  if (/\.title$/.test(pageTitle)) {
+    pageTitle = t('Layout.header')
+  }
+
+  return (
+    <Head>
+      <title>{pageTitle}</title>
+    </Head>
+  )
+}
+
+export default PageTitle
